79827b68e8330a8a575bb178943589fae8bf569d
Mikle <mmigalenko@mail.ru>
2020-08-03 18:15:54 +0300

    стилизация catalog.html tablet

--- source/js/filter-expand.js
+++ source/js/filter-expand.js
@@ -1,13 +1,18 @@
 var open = document.querySelector(".filter__expend-button");
 var filterExp = document.querySelector(".filter__filter-expended");
 var close = document.querySelector(".filter__close-expended-button");
+var countryList = document.querySelector(".filter__country-list");
+
+countryList.classList.add("filter__country-list_hidden-mobile");
 
 open.addEventListener("click", function () {
   open.classList.toggle("filter__expend-button_close");
   filterExp.classList.toggle("filter__filter-expended_hidden");
+  countryList.classList.toggle("filter__country-list_hidden-mobile");
 });
 
 close.addEventListener("click", function () {
   open.classList.toggle("filter__expend-button_close");
   filterExp.classList.toggle("filter__filter-expended_hidden");
+  countryList.classList.toggle("filter__country-list_hidden-mobile");
 });
