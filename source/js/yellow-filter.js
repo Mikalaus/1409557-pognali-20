b250bed8fa19f900ca9835be291101f68e68e78c
Mikle <mmigalenko@mail.ru>
2020-08-11 10:27:55 +0300

    Правки сетки и анимирование элементов

--- source/js/yellow-filter.js
+++ source/js/yellow-filter.js
@@ -1,8 +1,9 @@
 var contentBlockList = document.querySelectorAll(".companion-filter__content-block");
 var expendButtonsList = document.querySelectorAll(".companion-filter__expend-button");
+var expendList = document.querySelectorAll(".companion-filter__title-part");
 
-for (let i = 0; i < expendButtonsList.length; i++){
-  expendButtonsList[i].addEventListener("click", function () {
+for (let i = 0; i < expendList.length; i++){
+  expendList[i].addEventListener("click", function () {
     expendButtonsList[i].classList.toggle("companion-filter__expend-button_open");
     contentBlockList[i].classList.toggle("companion-filter__content-block_hidden");
   });
