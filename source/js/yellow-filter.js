392460a024f268cc9dbc26de29f671c00538048c
Mikle <mmigalenko@mail.ru>
2020-09-13 10:14:12 +0300

    внесение финальных правок

--- source/js/yellow-filter.js
+++ source/js/yellow-filter.js
@@ -1,10 +1,7 @@
 var contentBlockList = document.querySelectorAll(".companion-filter__content-block");
 var expendButtonsList = document.querySelectorAll(".companion-filter__expend-button");
 var expendList = document.querySelectorAll(".companion-filter__title-part");
-
-for (let i = 0; i < contentBlockList.length; i++){
-  contentBlockList[i].classList.toggle("companion-filter__content-block_hidden");
-}
+var contentBlockListHidden = document.querySelectorAll(".companion-filter__content-block_hidden");
 
 for (let i = 0; i < expendList.length; i++){
   expendList[i].addEventListener("click", function () {
