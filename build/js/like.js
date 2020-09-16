1fbdbb151937f262cd212ee1a9ff472d7060d455
Mikle <mmigalenko@mail.ru>
2020-09-16 11:44:06 +0300

    d

--- build/js/like.js
+++
@@ -1,7 +0,0 @@
-var likeList = document.querySelectorAll(".photo__like-button");
-
-for (let i = 0; i < likeList.length; i++){
-  likeList[i].addEventListener("click", function () {
-    likeList[i].classList.toggle("photo__like-button_active");
-  });
-}
